65a0c3986f0176850d85ccbe0aef062e4217e577
Artem Netsvetaev <cobal2m@gmail.com>
2025-12-01 02:49:51 +0300

    wip

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,26 +1,17 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-if (
-	!process.env.POSTGRES_DB ||
-	!process.env.POSTGRES_USER ||
-	!process.env.POSTGRES_PASSWORD ||
-	!process.env.POSTGRES_HOST ||
-	!process.env.POSTGRES_PORT
-) {
+const url = process.env.DATABASE_URL;
+
+if (url === undefined) {
 	throw new Error("Missing environment variables");
 }
 
 export default defineConfig({
+	dbCredentials: {
+		url,
+	},
+	dialect: "postgresql",
 	out: "./drizzle",
 	schema: "./src/database/schemas",
-	dialect: "postgresql",
-	dbCredentials: {
-		ssl: false,
-		database: process.env.POSTGRES_DB,
-		host: process.env.POSTGRES_HOST,
-		port: process.env.POSTGRES_PORT,
-		user: process.env.POSTGRES_USER,
-		password: process.env.POSTGRES_PASSWORD,
-	},
 });
--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 
-export const client = new Client({
+const client = new Client({
 	connectionString: process.env.DATABASE_URL,
 });
 
